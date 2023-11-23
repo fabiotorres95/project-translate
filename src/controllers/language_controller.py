34ab5e18c8a942b185a26d4db2dfe75e5781b5b0
fabiotorres95 <engfabiotorres@outlook.com>
2023-11-23 17:59:09 -0300

    created POST /

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 language_controller = Blueprint("languages", __name__)
 
@@ -13,4 +14,27 @@ def index():
         translate_from="pt",
         translate_to="en",
         translated="What do you want to translate?"
+        )
+
+
+@language_controller.route("/", methods=["POST"])
+def translate():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    translated = GoogleTranslator(
+        source=translate_from,
+        target=translate_to
+        ).translate(
+        text_to_translate
+        )
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated
         )
