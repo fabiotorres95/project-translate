43ac213a5457a5a106b60c5d3d76385f763aa554
fabiotorres95 <engfabiotorres@outlook.com>
2023-11-23 18:49:06 -0300

    created test_request_history

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,12 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    history = HistoryModel.list_as_json()
+    assert json.loads(history)[0]["translate_from"] == "en"
+    assert json.loads(history)[0]["translate_to"] == "pt"
+    assert json.loads(history)[1]["text_to_translate"] == "Do you love music?"
+    assert json.loads(history)[1]["translate_from"] == "en"
+    assert json.loads(history)[1]["translate_to"] == "pt"
