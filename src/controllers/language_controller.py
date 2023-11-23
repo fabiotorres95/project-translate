333ad90e7f166e535cbc7c15bb0d93eb525576dd
fabiotorres95 <engfabiotorres@outlook.com>
2023-11-23 18:05:06 -0300

    POST /reverse created

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -38,3 +38,26 @@ def translate():
         translate_to=translate_to,
         translated=translated
         )
+
+
+@language_controller.route("/reverse", methods=["POST"])
+def reverse():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    translated = GoogleTranslator(
+        source=translate_from,
+        target=translate_to
+        ).translate(
+        text_to_translate
+        )
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate
+        )
